755eb1259bf72da1b3f89babfb95bf404a1a6d44
salmaAla <164349788+salmaAla@users.noreply.github.com>
2025-12-10 07:49:24 +0100

    les reaction models.py

--- rooms/models.py
+++ rooms/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.conf import settings
 
 
 class Room(models.Model):
@@ -34,6 +35,36 @@ class Room(models.Model):
         return f"Room #{self.room_id} (par {self.creator.username})"
 
 
+class Reaction(models.Model):
+    """
+    Réactions dans une room (like, fun, wow, love).
+    """
+    REACTION_CHOICES = [
+        ("like", "Like"),
+        ("fun", "Fun"),
+        ("wow", "Wow"),
+        ("love", "Love"),
+    ]
+
+    room = models.ForeignKey(
+        Room,
+        on_delete=models.CASCADE,
+        related_name="reactions",
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    type = models.CharField(max_length=10, choices=REACTION_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("room", "user", "type")  # 1 réaction de chaque type par user
+
+    def __str__(self):
+        return f"{self.user} - {self.type} - {self.room.room_id}"
+
+
 class ImageQueue(models.Model):
     """
     File d'images à afficher dans une room.
@@ -63,10 +94,12 @@ class ImageQueue(models.Model):
         return f"Image {self.position} dans {self.room.room_id}"
 
 
-
-
 class Participant(models.Model):
-    room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name="participants")
+    room = models.ForeignKey(
+        Room,
+        on_delete=models.CASCADE,
+        related_name="participants",
+    )
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     joined_at = models.DateTimeField(auto_now_add=True)
 
