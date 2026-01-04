4340da2ef6577dda409556b946b17f5be2cbdfd6
Oussama401 <Oussamabeko118@gmail.com>
2026-01-04 02:47:07 +0100

    Update urls.py

--- rooms/urls.py
+++ rooms/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("join/", views.join_room, name="join_room"),
     path("<str:room_id>/", views.room_view, name="room_view"),
     path("<str:room_id>/set-mode/", views.set_mode, name="set_mode"),
+    path("<str:room_id>/set-image/", views.set_image, name="set_image"),
     path("<str:room_id>/leave/", views.leave_room, name="leave_room"),
 
     path("<str:room_id>/current-image/", views.current_image, name="current_image"),
