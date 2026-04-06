1c23cddfc7cc77866f03809457974d73ce2a19ac
SyphaxMEDJBER <syphax.medjber.8@gmail.com>
2026-04-06 22:56:04 +0200

    reactions supprimées : you are not gonna need it

--- rooms/routing.py
+++ rooms/routing.py
@@ -1,10 +1,8 @@
 from django.urls import re_path
 from .consumers import ChatConsumer, YouTubeConsumer
-from .consumers_reactions import ReactionConsumer
 
 
 websocket_urlpatterns = [
     re_path(r"ws/rooms/(?P<room_id>[0-9a-f]+)/$", ChatConsumer.as_asgi()),
     re_path(r"ws/youtube/(?P<room_id>[0-9a-f]+)/$", YouTubeConsumer.as_asgi()),
-    re_path(r"ws/reactions/(?P<room_id>[0-9a-f]+)/$", ReactionConsumer.as_asgi()),
 ]
